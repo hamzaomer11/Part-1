76a503a5cccbd737ff114a2f4b00cd197d70f486
hamesomer11 <hamzaomer45@gmail.com>
2024-04-19 10:31:08 +0100

    commit #3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total total={parseInt(exercises1 + exercises2 + exercises3)}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
@@ -29,9 +29,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.part1}, {props.exercises1}</p>
-      <p>{props.part2}, {props.exercises2}</p>
-      <p>{props.part3}, {props.exercises3}</p>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
     </div>
   )
 }
@@ -45,6 +45,5 @@ const Total = (props) => {
 
   )
 }
-
 
 export default App
