8b3f29d81d61c5e6be0ee2b11fe7b619f920733a
hamesomer11 <hamzaomer45@gmail.com>
2024-04-19 10:05:22 +0100

    commit #2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,19 +9,42 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total total={parseInt(exercises1 + exercises2 + exercises3)}/>
     </div>
   )
 }
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.part1}, {props.exercises1}</p>
+      <p>{props.part2}, {props.exercises2}</p>
+      <p>{props.part3}, {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3} </p>
+    </div>
+
+  )
+}
+
 
 export default App
