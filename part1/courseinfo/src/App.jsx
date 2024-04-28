5a4536bc02f3932558c6f1b3b3446e38bf4ed5f0
hamesomer11 <hamzaomer45@gmail.com>
2024-04-28 23:18:34 +0100

    commit -1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,10 @@
 const Hello = (props) => {
 
-  const name = props.name
-  const age = props.age
-
-
+  const { name, age } = props
   const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-
       <p>Hello {name}, you are {age} years old</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
