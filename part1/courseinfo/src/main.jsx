7a96d3de4da809cbb61e0703dec525b758442b58
hamesomer11 <hamzaomer45@gmail.com>
2024-04-29 21:12:24 +0100

    Page re-rendering - commit #2

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -4,6 +4,13 @@ import App from './App'
 
 let counter = 1
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App counter={counter} />
-)
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 1000)
