ee5da9855cbd5c37600e1bbf84772e25c9ea15b6
hamesomer11 <hamzaomer45@gmail.com>
2024-05-04 21:30:32 +0100

    Passing state - to child components - commit #2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,20 +5,24 @@ const App = () => {
 
 
   const increaseByOne = () => setCounter(counter + 1)
-  
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
       <Display counter={counter} />
-
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
@@ -26,6 +30,14 @@ const App = () => {
 const Display = (props) => {
   return (
     <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
