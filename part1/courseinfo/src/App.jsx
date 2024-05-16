7220d3f428afa023883a7720bef232b83f75ad5a
hamesomer11 <hamzaomer45@gmail.com>
2024-05-16 21:49:57 +0100

    A function that returns a function - commit #3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,20 +2,24 @@ import {useState} from 'react'
 
 const App = () => {
   const [value, setValue] = useState(10)
-  
 
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // print the new value to console
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
     setValue(newValue)
   }
-  
+
   return (
     <div>
       {value}
-
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
