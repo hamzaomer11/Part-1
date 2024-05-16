49aac2f0b7013a44456614a011ba477b942ad436
hamesomer11 <hamzaomer45@gmail.com>
2024-05-16 21:37:44 +0100

    A function that returns a function - commit #1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,16 +4,21 @@ const App = () => {
   const [value, setValue] = useState(10)
 
 
-  const handleClick = () => {
-    console.log('clicked the button')
-    setValue(0)
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
   }
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
+
 export default App
