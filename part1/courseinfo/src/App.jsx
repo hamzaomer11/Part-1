a142eff692546b8b41e4fc94ee1d2bb156d9fbb6
hamesomer11 <hamzaomer45@gmail.com>
2024-05-16 21:33:37 +0100

    Event Handling Revisited - commit #2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,8 +3,11 @@ import {useState} from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const handleClick = () =>
+
+  const handleClick = () => {
     console.log('clicked the button')
+    setValue(0)
+  }
 
   return (
     <div>
