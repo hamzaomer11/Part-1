0b9956d49e7a54c295f85394c4ad34732758a8b2
hamesomer11 <hamzaomer45@gmail.com>
2024-05-28 22:45:45 +0100

    unicafe: Exercise 1.6 /1.7 - commit #1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,37 @@
+/*********************************** Exercise 1.6 ******************************
+
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button> 
+      <button onClick={() => setBad(bad + 1)}>Bad</button> 
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h2>Statistics</h2>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+
+  )
+}
+
+export default App
+
+*/
+
+/*********************************** Exercise 1.7 *****************************
+ 
 import { useState } from 'react'
 
 const App = () => {
@@ -27,5 +61,6 @@ const App = () => {
   )
 }
 
-
 export default App
+
+*/
