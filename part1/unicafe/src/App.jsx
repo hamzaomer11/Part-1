2a3ac9ef26a649b0b1dbd38e61784d4afff4541c
hamesomer11 <hamzaomer45@gmail.com>
2024-06-01 19:32:07 +0100

    unicafe: Exercise 1.6/1.7/1.8/1.9/1.10/1.11 - commit #1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -232,3 +232,84 @@ const Statistics = (props) => {
 export default App
 
 */
+
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button handleClick={handleGoodClick} text={'Good'}/>
+      <Button handleClick={handleNeutralClick} text={'Neutral'}/>
+      <Button handleClick={handleBadClick} text={'Bad'}/>
+
+      <h2>Statistics</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Statistics good={good} neutral={neutral} bad={bad} all = {good + neutral + bad} average = {((good /good + neutral + bad) - (bad / good + neutral + bad))} positive = {(good/(good + neutral + bad)) * 100} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      
+    </div>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <div>
+      {text}
+      {value}
+    </div>
+  )
+
+}
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <StatisticLine text={'Good '} value={props.good}/>
+      <StatisticLine text={'Neutral '} value={props.neutral}/>
+      <StatisticLine text={'Bad '} value={props.bad}/>
+      <StatisticLine text={'All '} value={props.good + props.neutral + props.bad}/>
+      <StatisticLine text={'Average '} value={((props.good /(props.good + props.neutral + props.bad)) - (props.bad /(props.good + props.neutral + props.bad)))}/>
+      <StatisticLine text={'All (%) '} value={(props.good/(props.good + props.neutral + props.bad)) * 100} />    
+    </div>
+  )
+}
+
+export default App
